c8ea150f2e705e097270f437119ea9cf9b2f9d66
jihwooon <jihwooon@gmail.com>
2022-05-18 21:31:21 +0900

    test : shouldParseLinesFrom 테스트 코드 작성

--- app/src/test/java/Study/Java/chapter_02/cohesion/BankStatementCSVParserTest.java
+++ app/src/test/java/Study/Java/chapter_02/cohesion/BankStatementCSVParserTest.java
@@ -7,22 +7,40 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BankStatementCSVParserTest {
 
-     private final BankStatementParser statementParser = new BankStatementCSVParser();
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
     public void shouldParseOnCorrectLine() {
         final String line = "30-01-2017,-50,Tesco";
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
 
         final BankTransaction result = statementParser.parseFrom(line);
-        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
 
         assertThat(expected.getDate()).isEqualTo(result.getDate());
         assertThat(expected.getAmount()).isEqualTo(result.getAmount());
         assertThat(expected.getDescription()).isEqualTo(result.getDescription());
+    }
+
+    @Test
+    public void shouldParseLinesFrom() {
+        final String line = "30-01-2017,-50,Tesco";
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+
+        List<BankTransaction> result = statementParser.parseLinesFrom(List.of(line));
+
+        assertThat(result).isNotEmpty();
+
+        BankTransaction bankTransaction = result.get(0);
+
+        assertThat(bankTransaction.getDate()).isEqualTo(expected.getDate());
+        assertThat(bankTransaction.getAmount()).isEqualTo(expected.getAmount());
+        assertThat(bankTransaction.getDescription()).isEqualTo(expected.getDescription());
+
     }
 }
